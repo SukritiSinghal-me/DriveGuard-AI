93142f513f7bc439c6e60e341ebcd86b5d16bf0c
sukritisinghal19-crypto <sukritisinghal19@gmail.com>
2026-07-06 00:04:06 +0530

    alarm

--- utils/alarm.py
+++ utils/alarm.py
@@ -1,33 +1,8 @@
-import threading
-import winsound
-
-alarm_running = False
-
-
-def play_alarm():
-
-    global alarm_running
-
-    while alarm_running:
-        winsound.Beep(2000, 500)
-
+# Dummy alarm for deployment
 
 def start_alarm():
-
-    global alarm_running
-
-    if not alarm_running:
-
-        alarm_running = True
-
-        threading.Thread(
-            target=play_alarm,
-            daemon=True
-        ).start()
+    pass
 
 
 def stop_alarm():
-
-    global alarm_running
-
-    alarm_running = False
+    pass
